b2b95d25c49893269a99245b0bf49719e5c2d633
Virginiacxy <906033130@qq.com>
2017-10-05 14:34:51 -0700

    helloios with screenshots

---
+++ Hello-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello-iOS
+//
+//  Created by 陈馨玥 on 10/5/17.
+//  Copyright © 2017 Xinyue Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
